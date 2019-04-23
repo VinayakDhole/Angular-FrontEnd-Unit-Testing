0c1848a5a96a93bab0f5bbe856eb5dcad8050b8b
VinayakDhole <vinayak.dhole@verizonconnect.com>
2019-04-23 13:35:08 +0100

    correct the title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'FrontEnd-Unit-Tesing'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('FrontEnd-Unit-Tesing');
+    expect(app.title).toEqual('FrontEnd-Unit-Testing');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to FrontEnd-Unit-Tesing!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to FrontEnd-Unit-Testing!');
   });
 });
